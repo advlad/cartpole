5b729ba52259184f8fd37e1e1147ba7fc0029263
Vlad Adomnicai <vlad.adomnicai@gmail.com>
2019-09-24 09:29:37 +0300

    Update cartpole.py
    Replace MultiOutputRegressor with a single DecisionTreeRegressor.
    There is a bug regarding reward increasing.

--- cartpole.py
+++ cartpole.py
@@ -8,7 +8,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.linear_model import SGDRegressor
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.tree import DecisionTreeRegressor, export_graphviz
+from sklearn.tree import DecisionTreeRegressor, export_graphviz, DecisionTreeClassifier
 
 from scores.score_logger import ScoreLogger
 
@@ -19,7 +19,7 @@ LEARNING_RATE = 0.001 # unused as we use Experience Replay type of Q-Learning
 # See more on Experience Replay here: https://datascience.stackexchange.com/questions/20535/what-is-experience-replay-and-what-are-its-benefits
 
 MEMORY_SIZE = 5000 # used only for Non-Incremental learning, i.e. partial_fit=False
-BATCH_SIZE = 30
+BATCH_SIZE = 1000
 
 EXPLORATION_MAX = 1.0
 EXPLORATION_MIN = 0.05
@@ -33,23 +33,23 @@ class DQNSolver:
 
         self.action_space = action_space
         self.memory = deque(maxlen=MEMORY_SIZE)
-        self._is_partial_fit = is_partial_fit
-
-        if is_partial_fit:
-            # Here you can use only Incremental Models: https://scikit-learn.org/0.18/modules/scaling_strategies.html
-            regressor = SGDRegressor()
-            self.model = MultiOutputRegressor(regressor)
-        else:
-            # Here you can use whatever regression model you want, simple or Incremental
-            # The sklearn regression models can be found by searching for "regress" at https://scikit-learn.org/stable/modules/classes.html
-
-            # Ex:
-            #regressor = RandomForestRegressor(max_depth=2, random_state=0, n_estimators=100)
-            #regressor = LGBMRegressor(n_estimators=100, n_jobs=-1)
-
-            regressor = AdaBoostRegressor(n_estimators=10)
-            self.model = MultiOutputRegressor(regressor)
-
+        # self._is_partial_fit = is_partial_fit
+        self.model = DecisionTreeRegressor(min_samples_leaf=50)
+        # if is_partial_fit:
+        #     # Here you can use only Incremental Models: https://scikit-learn.org/0.18/modules/scaling_strategies.html
+        #     regressor = SGDRegressor()
+        #     self.model = MultiOutputRegressor(regressor)
+        # else:
+        #     # Here you can use whatever regression model you want, simple or Incremental
+        #     # The sklearn regression models can be found by searching for "regress" at https://scikit-learn.org/stable/modules/classes.html
+        #
+        #     # Ex:
+        #     #regressor = RandomForestRegressor(max_depth=2, random_state=0, n_estimators=100)
+        #     #regressor = LGBMRegressor(n_estimators=100, n_jobs=-1)
+        #
+        #     regressor = AdaBoostRegressor(n_estimators=10)
+        #     self.model = MultiOutputRegressor(regressor)
+        #
         self.isFit = False
 
     def remember(self, state, action, reward, next_state, done):
@@ -58,11 +58,14 @@ class DQNSolver:
     def act(self, state):
         if np.random.rand() < self.exploration_rate:
             return random.randrange(self.action_space)
-        if self.isFit == True:
-            q_values = self.model.predict(state)
-        else:
-            q_values = np.zeros(self.action_space).reshape(1, -1)
-        return np.argmax(q_values[0])
+        # if self.isFit == True:
+        #     q_values = self.model.predict(state)
+        # else:
+        #     q_values = np.zeros(self.action_space).reshape(1, -1)
+
+        prediction = self.model.predict(state)
+        threshold = 0.5
+        return 0 if prediction < threshold else 1
 
     def debug_batch(self, batch:List):
         X = []
@@ -77,29 +80,27 @@ class DQNSolver:
             if self.isFit:
                 q_values = self.model.predict(state)
             else:
-                q_values = np.zeros(self.action_space).reshape(1, -1)
-            q_values[0][action] = q_update
+                q_values = np.zeros(self.action_space).reshape(1, -1)[0][0]
+            q_values = q_values + q_update
 
             X.append(list(state[0]))
-            targets.append(q_values[0])
+            targets.append(q_values)
 
-        regressor = DecisionTreeRegressor(random_state=0)
-        model = MultiOutputRegressor(regressor)
+        model = DecisionTreeRegressor(random_state=0, min_samples_leaf=75)
+        #model = MultiOutputRegressor(regressor)
         clf = model.fit(X, targets)
-        my_regressor = model.estimators_[0]
-        export_graphviz(my_regressor, out_file='tree.dot')
+        export_graphviz(clf, out_file='model.dot',feature_names=['Position','Velocity','Angle','Velocity At Tip'])
+        #left_action_regressor = model.estimators_[0]
+        #right_action_regressor = model.estimators_[1]
+        # export_graphviz(left_action_regressor, out_file='left.dot',feature_names=['Position','Velocity','Angle','Velocity At Tip'])
+        # export_graphviz(right_action_regressor, out_file='right.dot',
+        #                 feature_names=['Position', 'Velocity', 'Angle', 'Velocity At Tip'])
         return
 
     def experience_replay(self):
         if len(self.memory) < BATCH_SIZE:
             return
-        if self._is_partial_fit:
-            batch = list(self.memory)
-            # if we use Incremental Model, we don't have to keep whole memory, as we use just last batch
-            # and the rest of the history is stored within the model, indirectly through learning
-            self.memory = deque(maxlen=BATCH_SIZE)
-        else:
-            batch = random.sample(self.memory, int(len(self.memory)/1))
+        batch = random.sample(self.memory, int(len(self.memory)/1))
         self.debug_batch(batch)
         X = []
         targets = []
@@ -113,16 +114,17 @@ class DQNSolver:
             if self.isFit:
                 q_values = self.model.predict(state)
             else:
-                q_values = np.zeros(self.action_space).reshape(1, -1)
-            q_values[0][action] = q_update
+                q_values = np.zeros(self.action_space).reshape(1, -1)[0][0]
+            q_values = q_values+q_update
             
             X.append(list(state[0]))
-            targets.append(q_values[0])
+            targets.append(q_values)
 
-        if self._is_partial_fit:
-            self.model.partial_fit(X, targets)
-        else:
-            self.model.fit(X, targets)
+        # if self._is_partial_fit:
+        #     self.model.partial_fit(X, targets)
+        # else:
+        #     self.model.fit(X, targets)
+        self.model.fit(X, targets)
 
         self.isFit = True
         self.exploration_rate *= EXPLORATION_DECAY
